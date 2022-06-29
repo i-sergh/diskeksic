188727db120685e47e98e10297d4a0091933f9d9
Sergey Ippolitov <seripp28@gmail.com>
2022-06-30 00:02:10 +0300

    made signup

--- core/views.py
+++ core/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+#from cProfile import Profile
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
+from .models import Profile
+
+
+
+
+
 # Create your views here.
 def index(reqest):
     #return HttpResponse('<H1>Дискексик</H1>')
@@ -7,4 +16,32 @@ def index(reqest):
 
 
 def signup(request):
-    return render(request, 'signup.html')
+    
+    if request.method == 'POST':
+
+        username =  request.POST['username']
+        email =  request.POST['email']  
+        password =  request.POST['password'] 
+        confirm_password =  request.POST['confirm_password'] 
+
+        if password == confirm_password:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username,email=email, password=password)
+                user.save()
+                print(username)
+                user_model = User.objects.get(username)
+                profile = Profile.objects.create(user=username, id_user=user_model.id)
+                profile.save()
+                return redirect('signup')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')

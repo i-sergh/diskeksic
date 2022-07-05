2f5936ef0954c8852b217888092273279e1c0ec3
Sergey Ippolitov <seripp28@gmail.com>
2022-07-05 18:12:20 +0300

    created user settings form

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 #from cProfile import Profile
+import re
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, auth
 from django.contrib.auth.decorators import login_required
@@ -20,8 +21,31 @@ def index(reqest):
 
     
 @login_required(login_url='signin')
-def settings(request):
-    return render(request, 'user_settings.html') 
+def user_settings(request):
+    user_profile = Profile.objects.get(user=request.user)
+    user = User.objects.get(username=request.user)
+    
+    if request.method =='POST':
+
+        if request.FILES.get('profile_img') == None:
+            image = user_profile.profileimg
+        else:
+            image = request.FILES.get('profile_img')
+        
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        bio = request.POST['bio']
+        location = request.POST['location']
+        
+        user.first_name = first_name
+        user.last_name = last_name
+        user_profile.profileimg = image
+        user_profile.bio = bio
+        user_profile.location = location
+        user_profile.save()
+        user.save()
+        return redirect('user_settings')
+    return render(request, 'user_settings.html', {'user_profile': user_profile}) 
 
 def signup(request):
     
@@ -52,7 +76,10 @@ def signup(request):
                 
             profile = Profile.objects.create(user=user_model, id_user=user_model.id)
             profile.save()
-            return redirect('signin')
+
+            user_login = auth.authenticate(username=username, password=password)
+            auth.login(request, user_login)
+            return redirect('user_settings')
         
         
     else:
